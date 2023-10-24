0a653b5f99d232aea7ea47957c3d8e8ef955d359
jangomax <jangomaxstudio@gmail.com>
2023-10-23 22:41:45 -0700

    admin/reset

--- src/api/admin.py
+++ src/api/admin.py
@@ -20,22 +20,31 @@ def reset():
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
     with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("DELETE FROM item_ledger"))
+        connection.execute(sqlalchemy.text("DELETE FROM ml_ledger"))
+        connection.execute(sqlalchemy.text("DELETE FROM gold_ledger"))
+        connection.execute(sqlalchemy.text("DELETE FROM transactions"))
+        connection.execute(sqlalchemy.text("DELETE FROM cart_item"))
+        connection.execute(sqlalchemy.text("DELETE FROM carts"))
+
+        connection.execute(sqlalchemy.text("ALTER SEQUENCE transactions_id_seq RESTART WITH 1"))
+        connection.execute(sqlalchemy.text("ALTER SEQUENCE gold_ledger_id_seq RESTART WITH 1"))
+        connection.execute(sqlalchemy.text("ALTER SEQUENCE ml_ledger_id_seq RESTART WITH 1"))
+        connection.execute(sqlalchemy.text("ALTER SEQUENCE item_ledger_id_seq RESTART WITH 1"))
+        connection.execute(sqlalchemy.text("ALTER SEQUENCE carts_n_id_seq RESTART WITH 1"))
+
+        transaction_id = connection.execute(sqlalchemy.text(
+            """
+            INSERT INTO transactions (description)
+            VALUES (:desc) RETURNING id
+            """), {"desc": "Init"}).scalar_one()
         connection.execute(sqlalchemy.text(
             """
-            UPDATE global_inventory SET
-            gold = 100,
-            num_red_ml = 0,
-            num_green_ml = 0,
-            num_blue_ml = 0,
-            num_dark_ml = 0
-            """
-        ))
-        connection.execute(sqlalchemy.text(
-            """
-            UPDATE catalog_item SET
-            quantity = 0
-            """
-        ))
+            INSERT INTO gold_ledger (change, transaction_id)
+            VALUES (:change, :transaction_id)
+            """), {"change": 100, "transaction_id": transaction_id})
+
+
     return "OK"
 
 
